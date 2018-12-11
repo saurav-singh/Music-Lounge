f5ff9b99b779ee6e68e62e0a2bb89c2eab7f8455
Taylor Glietz <teg53@drexel.edu>
2018-12-11 08:54:32 -0500

    updated demo schema

---
+++ Dump20181211/music_lounge_usertable.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+--
+-- Host: localhost    Database: music_lounge
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `usertable`
+--
+
+DROP TABLE IF EXISTS `usertable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `usertable` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) DEFAULT NULL,
+  `password` char(65) DEFAULT NULL,
+  `profilePictureLocation` varchar(60) DEFAULT NULL,
+  `genre` varchar(30) DEFAULT NULL,
+  `originCity` varchar(50) DEFAULT NULL,
+  `activeSince` year(4) DEFAULT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usertable`
+--
+
+LOCK TABLES `usertable` WRITE;
+/*!40000 ALTER TABLE `usertable` DISABLE KEYS */;
+INSERT INTO `usertable` VALUES (1,'taylor','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08','DefaultProfile.PNG','Rock','Philadelphia',2000),(2,'Metallica','336665b051dbac2e59b32911c20f519a1d4fa75d65407207d2e4a4d09e762275','Metallica.png','Heavy Metal','Los Angeles, CA',1981),(3,'OzzyOsbourne','b579d6ddaea9eb9bf4caa199451a84d2b35d775cc4bc989cf211061524f5b0f3','Ozzy.png','Heavy Metal','Birmingham, England',1967),(4,'ReelBigFish','e6db64b54c01ae7bcae71a7c9dbbbce8b8d65800d045050c7a6addad049d7c6b','ReelBigFish.png','Ska','Los Alamitos, CA',1991),(5,'SuburbanLegends','a27154809adae3840a76308e216d779b3da70b344b86ffe54b9108442b57ae0a','SuburbanLegends.png','Ska','Huntington Beach, CA',1998),(6,'Flogging Molly','440f3041c89adee0f2ad780704bcc0efae1bdb30f8d77dc455a2f6c823b87ca0','LifeIsGood.png','Folk Rock','Los Angeles, CA',1997);
+/*!40000 ALTER TABLE `usertable` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-12-11  8:50:02
